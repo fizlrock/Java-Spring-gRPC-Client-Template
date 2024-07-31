464c985adf9e6c372754c26e25423afbb0b6990a
fizlrock <nirku0soft@yandex.ru>
2024-07-31 06:57:05 +0400

    init

---
+++ app/src/main/java/dev/fizlrock/App.java
@@ -0,0 +1,15 @@
+package dev.fizlrock;
+
+import dev.fizlrock.ears.proto.EarsServiceGrpc.EarsServiceBlockingStub;
+
+/**
+ * App
+ */
+public class App {
+
+  public static void main(String[] args) {
+
+  	System.out.println("hey hey");
+  }
+	
+}
